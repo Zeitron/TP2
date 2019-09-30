509b59443b417b9b9f418b36c75f5f63e825f582
Zeitron <mein.zeitron@gmail.com>
2019-09-30 01:15:24 -0300

    scraper

--- main/src/searchEngine/scraper.rb
+++ main/src/searchEngine/scraper.rb
@@ -3,9 +3,13 @@ require 'httparty'
 require 'byebug'
 
 def scraper
-  url = "https://www.ranks.nl/stopwords/brazilian"
+  #url = "https://project.centeias.net/en_US/"
+  #url = "https://www.kaggle.com/rtatman/stopword-lists-for-19-languages#portugueseST.txt"
+  url = "https://www.yogajournal.com/poses/types"
   raw_page = HTTParty.get(url)
   page = Nokogiri::HTML(raw_page)
+  yoga = page.css('h2.m-card--header-text').text
+  #stopwords = page.css('div.jqvxeM').text
   byebug
 end
 
