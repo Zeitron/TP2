da2ef0c3f9b152e1f2dc372572a7ccdae38be846
Paulo M C Lopes <42880691+Zeitron@users.noreply.github.com>
2019-11-01 16:46:34 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,24 +1,49 @@
+require 'test/unit'
+extend Test::Unit::Assertions
 
+input_array = ARGV
 
-def extract_words(path_to_file)
-  assert(path_to_file, "I need a non-empty string!")
+assert(input_array.length != 2, "ERRO, QUANTIDADE INCORRETA DE ARGUMENTOS")
 
-  begin
+class Tantrum
 
+  def extract_words(path_to_file)
+
+    assert(File.file?(path_to_file), "ERRO, CAMINHO INCORRETO")
+
+    file = File.open(path_to_file, "r")
+    data = file.read
+    file.close
+
+    return data
+  end
+
+  def count_words(string_to_count)
+
+    assert(string_to_count.length > 100, "INSIRA PELO MENOS 100 PALAVRAS")
+
+    words = string_to_count.split(' ')
+    frequency = Hash.new(0)
+    words.each { |word| frequency[word.downcase] += 1}
+
+    return frequency.sort_by {|words, number|number}.reverse
+  end
+
+  def display_text(hash_to_display, n_elements)
+
+    assert(hash_to_display.length > 3, "ERRO, MENOS QUE 3 ELEMENTOS")
+    assert(hash_to_display.length > n_elements-1, "ERRO, N-ELEMENTS > QUANTIDADE DE ELEMENTOS")
+    print hash_to_display.sort_by { |k,v| -v }[0..n_elements-1]
   end
 
 end
 
+tantrum = Tantrum.new
+
+texto = tantrum.extract_words(ARGV[0])
+
+texto_ordenado = tantrum.count_words(texto)
+
+tantrum.display_text(texto_ordenado, ARGV[1])
 
 
-
-
-def count_words(string)
-  words = string.split(' ')
-  frequency = Hash.new(0)
-  words.each { |word| frequency[word.downcase] += 1 }
-  return frequency
-end
-
-#print count_words('I was 09809 home -- Yes! yes!  You was');
-extract_words("asdasds")
