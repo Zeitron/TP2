7229cedf67981c7ac91a66d666578e92ad82aca2
Paulo M C Lopes <42880691+Zeitron@users.noreply.github.com>
2019-11-07 03:32:31 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,49 +1,81 @@
 require 'test/unit'
 extend Test::Unit::Assertions
 
-input_array = ARGV
+#RETIRA OS DADOS DO TXT
+def extract_words(path_to_file)
 
-assert(input_array.length != 2, "ERRO, QUANTIDADE INCORRETA DE ARGUMENTOS")
+  assert(File.file?(path_to_file), "ERRO, CAMINHO INCORRETO")
 
-class Tantrum
+  file = File.open(path_to_file, "r")
+  data = file.read
+  file.close
 
-  def extract_words(path_to_file)
+  return data
+end
 
-    assert(File.file?(path_to_file), "ERRO, CAMINHO INCORRETO")
 
-    file = File.open(path_to_file, "r")
-    data = file.read
-    file.close
+#CONTA QUANTAS PALAVRAS EXISTEM
+def count_words(string_to_count)
 
-    return data
-  end
+  assert(string_to_count.length > 10, "INSIRA PELO MENOS 10 PALAVRAS")
 
-  def count_words(string_to_count)
+  words = string_to_count.split(' ')
+  frequency = Hash.new(0)
+  words.each { |word| frequency[word.downcase] += 1}
 
-    assert(string_to_count.length > 100, "INSIRA PELO MENOS 100 PALAVRAS")
+  return frequency.sort_by {|words, number|number}.reverse
+end
 
-    words = string_to_count.split(' ')
-    frequency = Hash.new(0)
-    words.each { |word| frequency[word.downcase] += 1}
 
-    return frequency.sort_by {|words, number|number}.reverse
-  end
+#MOSTRA N ELEMENTOS
+def display_text(hash_to_display, n_elements)
 
-  def display_text(hash_to_display, n_elements)
+  n_elements = n_elements.to_i
 
-    assert(hash_to_display.length > 3, "ERRO, MENOS QUE 3 ELEMENTOS")
-    assert(hash_to_display.length > n_elements-1, "ERRO, N-ELEMENTS > QUANTIDADE DE ELEMENTOS")
-    print hash_to_display.sort_by { |k,v| -v }[0..n_elements-1]
-  end
+  assert(hash_to_display.length > 3, "ERRO, MENOS QUE 3 ELEMENTOS")
+  assert(hash_to_display.length > n_elements - 1, "ERRO, N-ELEMENTS > QUANTIDADE DE ELEMENTOS")
+
+  return hash_to_display.sort_by { |k,v| -v }[0..n_elements-1]
+end
+
+
+#REMOVE STOPWORDS
+def remove_stopwords(stopword, text)
+
+  assert(stopword.length > 0, "Stopwords vazio")
+
+  array_stop = stopword.split("\n")
+
+  array_text = text.split.join(" ")
+  array_text = array_text.gsub(/[-#*.;,+:"]/, "")         # removo caracteres especiais
+  array_text = array_text.gsub('\\"', "")
+
+  #pattern = /\b(?:#{ Regexp.union(array_stop).source })\b/
+
+  pattern = /\b(?:#{ array_stop.join('|') })\b/i
+
+  #return array_text.gsub(pattern, '').squeeze(' ').strip
+
+  return array_text.gsub(pattern, '')
 
 end
 
-tantrum = Tantrum.new
-
-texto = tantrum.extract_words(ARGV[0])
-
-texto_ordenado = tantrum.count_words(texto)
-
-tantrum.display_text(texto_ordenado, ARGV[1])
 
 
+
+input_array = ARGV
+
+assert(input_array.length == 2, "ERRO, QUANTIDADE INCORRETA DE ARGUMENTOS")
+
+texto = extract_words(ARGV[0])
+
+stopwords = extract_words("stop_words.txt")
+
+# Teste de Erro pra stopwords vazio
+# stopwords = extract_words("vazio.txt")
+
+text_sem_stop = remove_stopwords(stopwords, texto)
+
+texto_ordenado = count_words(text_sem_stop)
+
+print display_text(texto_ordenado, ARGV[1])
